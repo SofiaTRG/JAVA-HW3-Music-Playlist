6166bbad7f6cf6e0332fa46e6851a6b71c983d8c
Sofia Torgovezky <sofiat@campus.technion.ac.il>
2023-06-12 14:33:57 +0300

    11/06: - Started the class song.

--- src/Playlist.java
+++ src/Playlist.java
@@ -7,8 +7,16 @@ public class Playlist extends Iterable<T> implements Cloneable {
     private static ArrayList<Song> playlist;
 
     public Playlist(Song... songs) {
-
+        for (Song song: songs) {
+            playlist.add(song);
+        }
     }
+
+    // ???????
+    public static void setPlaylist(ArrayList<Song> playlist) {
+        Playlist.playlist = playlist;
+    }
+
     @Override
     public Iterator iterator() {
         return null;
