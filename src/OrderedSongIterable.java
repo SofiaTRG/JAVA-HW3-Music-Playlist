5ec6692d0fde99c6339f9590be8f405f4d870fef
Sofia Torgovezky <sofiat@campus.technion.ac.il>
2023-06-24 21:36:27 +0300

    16/06: -Most of the code working correctly. need to fix small issues.

--- src/OrderedSongIterable.java
+++ src/OrderedSongIterable.java
@@ -1,3 +1,11 @@
-public interface OrderedSongIterable extends Iterable<Song>{
+/**
+ * Represents an iterable collection of songs with ordering capabilities.
+ */
+public interface OrderedSongIterable extends Iterable<Song> {
+
+    /**
+     * Sets the scanning order of the songs in the collection.
+     * @param order the scanning order to set
+     */
     public void setScanningOrder(ScanningOrder order);
 }
